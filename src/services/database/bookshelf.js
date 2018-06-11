06c011a1bb3d335990ef8c60870f93ca25bc0657
Vadym <zhufyakvv@gmail.com>
2018-06-11 11:32:39 +0300

    Replaced Flter and OrderBy by lib.

--- src/services/database/bookshelf.js
+++ src/services/database/bookshelf.js
@@ -2,6 +2,7 @@
  * Knex and bookshelf configuration file. Bookshelf plugins and other tasty stuff should go here.
  */
 
+import bcqm from "bookshelf-collection-querystring-mutation";
 import bookshelf from "bookshelf";
 import config from "../../../knexfile";
 import k from "knex";
@@ -11,5 +12,6 @@ const ORM = bookshelf(knex);
 
 ORM.plugin("visibility");
 ORM.plugin("pagination");
+ORM.plugin(bcqm);
 
 export default ORM;
